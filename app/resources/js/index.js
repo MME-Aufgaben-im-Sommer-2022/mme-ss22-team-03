ef96596b3e7592393fde2a7cb53f3cf9b78d6d85
huabahubre <samuel.huber@mendi.io>
2022-09-06 11:27:39 +0200

    Added MapManager.js

---
+++ app/resources/js/index.js
@@ -0,0 +1,11 @@
+import { MapManager } from "../js/modules/MapManager.js";
+
+let myMapManager;
+
+
+function init()
+{
+    let myMapManager = new MapManager();
+}
+
+init();
--- app/resources/js/modules/MapManager.js
+++ app/resources/js/modules/MapManager.js
@@ -4,7 +4,7 @@ var maxMapZoom = 19,
 
 var map;
 
-function initManager() {
+function initManager(map) {
     map = L.map('map').setView([43.624289, 11.884090], 17);
 
     L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
@@ -15,9 +15,35 @@ function initManager() {
     //console.log("Finished Init Map Manager");
 }
 
+function initMarkers(map)
+{
+    var marker = L.marker([43.624289, 11.884090]).addTo(map);
+}
+
 function setZoomState(zoom)
 {
     map.setZoom(zoom);
 }
 
 initManager();
+initMarkers();
+
+
+class MapManager extends Observable {
+
+    constructor() {
+        super();
+
+        this.maxMapZoom = 19,
+        this.currentCoordinateX = 43.624289,
+        this.currentCoordinateY = 11.884090;
+
+        this.map = L.map('map').setView([43.624289, 11.884090], 17);
+        initManager(this.map);
+        initMarkers(this.map);
+    }
+
+}
+
+
+export default MapManager;
