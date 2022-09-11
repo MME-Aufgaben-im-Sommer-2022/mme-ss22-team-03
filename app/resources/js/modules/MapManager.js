fe942eba5ccb9766a71b454555962831bd6d1f34
huabahubre <hubsamu@hotmail.de>
2022-09-11 21:47:58 +0200

    Finished Marker Switch

--- app/resources/js/modules/MapManager.js
+++ app/resources/js/modules/MapManager.js
@@ -1,19 +1,15 @@
 function initManager(manager) {
 
-  manager.map = L.map('map').setView([manager.currentCoordinateX, manager
-    .currentCoordinateY
-  ], manager.currentZoom);
+  manager.map = L.map('map').setView(manager.startCoords, manager.currentZoom);
 
   L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: manager.maxMapZoom,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
   }).addTo(manager.map);
-
-  console.log("Finished Init Map Manager");
 }
 
 //TODO: Differate between Pastina Markes and Surrounding Markers
-function initMarkers(manager, markerCoordList) {
+function initMarkers(manager, placeList) {
 
   var Icon = L.icon({
     iconUrl: './resources/images/map_page/marker.png',
@@ -27,43 +23,38 @@ function initMarkers(manager, markerCoordList) {
     ] // point from which the popup should open relative to the iconAnchor
   });
 
-  for (var i = 0; i < markerCoordList.length; i += 2) {
-    var marker = L.marker([markerCoordList[i], markerCoordList[i +
-      1]], { icon: Icon }).addTo(manager.map);
+  for (var i = 0; i < placeList.length; i += 1) {
+    var marker = L.marker(placeList[i].coords, { icon: Icon }).addTo(manager.map);
     manager.PastinaMarkerList.push(marker);
   }
-
-  console.log(marker.getLatLng());
 }
 
 
 class MapManager {
 
-  constructor(pastinaCoordList, surroundingCoordList, maxZoom, startZoom,
-    startCoordX, startCoordY) {
+  constructor(placeList, maxZoom, startZoom,
+    startCoords) {
 
-    this.pastinaCoordList = pastinaCoordList;
-    this.surroundingCoordList = surroundingCoordList;
+    this.placeList = placeList;
 
     this.maxMapZoom = maxZoom,
       this.currentZoom = startZoom;
-    this.currentCoordinateX = startCoordX,
-      this.currentCoordinateY = startCoordY;
+    this.startCoords = startCoords,
 
     this.PastinaMarkerList = [];
 
     //this.map = L.map('map').setView([43.624289, 11.884090], 17);
 
     initManager(this);
-    initMarkers(this, pastinaCoordList);
+    initMarkers(this, this.placeList);
   }
 
   setZoomState(zoom) {
     this.map.setZoom(zoom);
   }
 
-  flyTo(coordX, coordY, zoom) {
-    this.map.flyTo([coordX, coordY], zoom);
+  flyTo(coords, zoom) {
+    this.map.flyTo(coords, zoom);
   }
 
 }
