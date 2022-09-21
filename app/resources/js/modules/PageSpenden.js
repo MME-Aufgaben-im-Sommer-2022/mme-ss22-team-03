c7ebc942a0908557c419368030fbf0bd50a3a818
dena00 <dena.mehr@stud.uni-regensburg.de>
2022-09-21 21:01:45 +0200

    spenden social media button
    open a new page

--- app/resources/js/modules/PageSpenden.js
+++ app/resources/js/modules/PageSpenden.js
@@ -1,11 +1,24 @@
-import SpendenFormManager from "./SpendenFormManager.js"
+import SpendenFormManager from "./SpendenFormManager.js";
+
+var text = encodeURIComponent("Follow Jeep form Amazing JavaScript Tutorial");
+var url= "https://medium.com/@jagathishsaravanan/";
+var user_id = "jagathish1123";
+var hash_tags = " JS,JavaScript, 100DaysOfCode, Programming";
+var params = "menubar=no, toolbar=no, status=no, width=570, height=570"; //for window
+var facebook = document.createElement('.facebookBtn');
 
 let mySpendenHelper;
+
 
 function init()
 {
     mySpendenHelper = new SpendenFormManager();
+  
 }
 
+facebook.addEventListener('click', function(ev){
+    console.log("Hat geklappt");
 
-init();
+    let shareUrl = 'http://www.facebook.com/sharer/sharer.php?u=${url}';
+    window.open(shareUrl, "NewWindow", params);
+});
