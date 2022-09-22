dd34606de2ec848663569be6ea44ab00f4a04506
huabahubre <samuel.huber@mendi.io>
2022-09-22 13:12:06 +0200

    Happening html & LOist wokring

--- app/resources/js/modules/module_event.js
+++ app/resources/js/modules/module_event.js
@@ -1,10 +1,22 @@
 function InitHappening(happening) {
-    // TODO: create Template from html template
-    // var newHappening = document.getElementById("happeningItem");
 
-    // Fill Template with Information -> happening.data.EXAMPLEDATA
-    // newHappening.get("HeaderID") = happening.data.header;
-    // newHappening.getAttribute("SubheaderID") = happening.data.subheader;
+    const happeningElement01 = happening.clone.querySelector('.happeningElement01');
+    const happeningElement02 = happening.clone.querySelector('.happeningElement02');
+
+    let htmlClone;
+
+    if (happening.index % 2 == 0) {
+        htmlClone = happeningElement01.cloneNode(true)
+    } else {
+        htmlClone = happeningElement02.cloneNode(true)
+    }
+
+    htmlClone.querySelector('.Header').textContent = happening.data.header;
+    htmlClone.querySelector('.Subheader').textContent = happening.data.subheader;
+    htmlClone.querySelector('.Content').textContent = happening.data.content;
+    htmlClone.querySelector('.Image').src = happening.data.imageSrc;
+
+    happening.htmlData = htmlClone;
 
     // Get All Buttons from created Template
     // happening.controls = {
@@ -19,10 +31,14 @@ function InitHappening(happening) {
 }
 
 class Happening {
-    constructor(type, data) {       //type: 0 = Event, 1 = Project // data = { .header, .time, .content, .image}
+    constructor(type, data, index, htmlClone) {       //type: 0 = Event, 1 = Project // data = { .header, .time, .content, .image}
 
         this.type = type;
         this.data = data;
+        this.index = index;
+        this.clone = htmlClone;
+
+        this.htmlData = null;
 
         InitHappening(this);
     }
--- app/resources/js/pages/PageManager_Events.js
+++ app/resources/js/pages/PageManager_Events.js
@@ -9,43 +9,67 @@ function initManager(manager) {
 
     initControls(manager);
     initEventList(manager);
+
+    UpdateHappeningList(manager);
 }
 
 function initControls(manager) {
 
-    // manager.controls = {
-    //     exampleButton: document.getElementsByName('exampleID')[0],
-    // }
+    manager.controls = {
+        happeningList: document.querySelector('.happeningList'),
+        overViewList: document.querySelector('.overViewList'),
+    }
 
-    // manager.controls.exampleButton.addEventListener("click", function (
-    //     e) {
-    //         manager.exampleFunction("value");
-    //   });
+    const template = document.querySelector('#happeningTemplate');
+    manager.clone = template.content.cloneNode(true);
+    manager.overViewElement = manager.clone.querySelector('.overViewElement')
 }
 
 function initEventList(manager) {
+
     //TODO: Fetch Event Data from SQL -> Hardcoded first
 
     var OlivenErnteData = {
         header: "Oliven Ernte",
-        subheader: "Test Subheader",
-        content: "Test Content",
-        image: "Test Image URL",
+        subheader: "xx.xx.xxxx - xx.xx.xxxx",
+        content: "Hallo das ist ein Test Content",
+        imageSrc: "./resources/images/event_page/IMG_event_olivenernte.png",
     }
-    var OlivenErnteEvent = new Happening("Event", OlivenErnteData);
-    manager.eventList.push(OlivenErnteEvent);
+    var OlivenErnteEvent = new Happening("Event", OlivenErnteData, manager.happeningList.length,  manager.clone);
+    manager.happeningList.push(OlivenErnteEvent);
 
 
     var ExampleHappeningData2 = {
         header: "Test Header2",
         subheader: "Test Subheader2",
         content: "Test Content2",
-        image: "Test Image URL2",
+        imageSrc: "./resources/images/event_page/IMG_event_permakultur.png",
     }
-    var ExampleHappening2 = new Happening("Event", ExampleHappeningData2);
-    manager.eventList.push(ExampleHappening2);
+    var ExampleHappening2 = new Happening("Event", ExampleHappeningData2, manager.happeningList.length, manager.clone);
+    manager.happeningList.push(ExampleHappening2);
 
-    console.log(manager.eventList);
+
+    var ExampleHappeningData2 = {
+        header: "Test Header2",
+        subheader: "Test Subheader2",
+        content: "Test Content2",
+        imageSrc: "./resources/images/event_page/IMG_event_permakultur.png",
+    }
+    var ExampleHappening3 = new Happening("Event", ExampleHappeningData2, manager.happeningList.length, manager.clone);
+    manager.happeningList.push(ExampleHappening3);
+}
+
+function UpdateHappeningList(manager) {
+
+    manager.happeningList.forEach(happening => {
+
+        let overViewClone = manager.overViewElement.cloneNode(true);
+        overViewClone.querySelector('.overViewHeader').textContent = happening.data.header;
+        overViewClone.querySelector('.overViewSubheader').textContent = happening.data.subheader;
+
+        manager.controls.overViewList.append(overViewClone);
+        manager.controls.happeningList.append(happening.htmlData);
+    })
 }
 
 class PageManager_Events extends Observable {
@@ -53,10 +77,9 @@ class PageManager_Events extends Observable {
     constructor() {
         super();
 
-        this.eventList = [];
+        this.happeningList = [];
 
         initManager(this);
-        initEventList(this);
     }
 
     exampleFunction() {
