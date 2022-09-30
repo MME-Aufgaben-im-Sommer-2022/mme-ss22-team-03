59513fa146c48ee3dd86cfb6a8f3906247f779c7
dena00 <dena.mehr@stud.uni-regensburg.de>
2022-09-30 19:45:13 +0200

    shareButton

--- app/src/js/modules/ButtonManager.js
+++ app/src/js/modules/ButtonManager.js
@@ -1,5 +1,9 @@
 import { Event, Observable } from "../utils/Observable.js";
 
+const postTitle = ("Hi schau dir mal die coole Seite an :D ");
+
+ let postUrl = document.location.href;
+
 function init(manager) {
 
     initButtonList(manager);
@@ -8,6 +12,7 @@ function init(manager) {
 
 function initButtonList(manager) {
     manager.buttonHTMLList = document.querySelectorAll(".button");
+    console.log(manager.buttonHTMLList);
 }
 
 function initListeners(manager) {
@@ -28,6 +33,7 @@ class ButtonManager extends Observable {
     buttonClick(id, name) {
         let event;
 
+        console.log(id);
         switch (id) {
             case "donate":
                 // eslint-disable-next-line no-case-declarations
@@ -44,11 +50,40 @@ class ButtonManager extends Observable {
             case "scroll":
                 event = new Event(id, name);
                 break;
+            case "share":
+                this.shareButton(name);
+                break;
             default:
                 break;
         }
 
         this.notifyAll(event);
+    }
+
+    shareButton(name) {
+        console.log(name);
+        switch(name) {
+            
+            case "whatsapp":
+                
+                window.location = "https://wa.me/?text=${postTitle} ${postUrl}";
+                //TODO: hier auf eine url verweisen -> weiterleiten
+                break;
+            case "mailBtn":
+                window.location = "https://plus.google.com/share?url=[post-url]";
+                break;
+            case "facebook":
+                window.location = "https://www.facebook.com/sharer.php?u=[post-url]";
+                break;
+            case "linkedInBtn":
+                window.location = "https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]";
+                break;
+            case "instagram":
+                window.location = "https://ig.me/m/";
+                break;
+            default:
+                break;
+        }
     }
 }
 
